23c2c5d8ff353b78683a99ecfe23a7419a81312d
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-07-11 17:39:48 +0545

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://mega-study-notion.vercel.app",
 		credentials:true,
 	})
 )
