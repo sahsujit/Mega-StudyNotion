edc119a00b4725fe8fea584e02da187fbc7782b1
꧁༺քʀօɢʀǟʍʍɛʀ ֆʊʝɨȶ༻꧂ <129708170+sahsujit@users.noreply.github.com>
2024-07-13 12:47:15 +0545

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 const getBaseUrl = () => {
     if (process.env.NODE_ENV === 'production') {
-      return "https://studynotion-backend-ro0r.onrender.com/api/v1";
+      return "https://mega-studynotion.onrender.com/api/v1";
     }
     return "http://localhost:4000/api/v1";
   };
